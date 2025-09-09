b4233f39d0be79ecad61d95ae4e73993bbf4c4a7
ShaunRussell27 <shaunrussell1111@gmail.com>
2025-09-09 14:16:40 +0100

    Add files via upload

--- signup.js
+++ signup.js
@@ -9,7 +9,7 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvi
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBxh0T0Kan7qvQyFVzpu0p-zWHwETjIzSY",
+  //apiKey: "AIzaSyBxh0T0Kan7qvQyFVzpu0p-zWHwETjIzSY",
   authDomain: "wheretofindai.firebaseapp.com",
   projectId: "wheretofindai",
   storageBucket: "wheretofindai.firebasestorage.app",
